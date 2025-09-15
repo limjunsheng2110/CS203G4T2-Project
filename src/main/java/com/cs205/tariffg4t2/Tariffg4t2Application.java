75ab9f886ff8b8f5c00873e33c1b14b2f37ce54f
Kayweesim <kaywee.sim.2024@smu.edu.sg>
2025-09-15 20:03:08 +0800

    Added Folders for project structure

--- src/main/java/com/cs205/tariffg4t2/Tariffg4t2Application.java
+++ src/main/java/com/cs205/tariffg4t2/Tariffg4t2Application.java
@@ -2,12 +2,21 @@ package com.cs205.tariffg4t2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
 
 @SpringBootApplication
-public class Tariffg4t2Application {
+public class Tariffg4t2Application implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(Tariffg4t2Application.class, args);
+    }
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Testing");
+
+
     }
 
 }
