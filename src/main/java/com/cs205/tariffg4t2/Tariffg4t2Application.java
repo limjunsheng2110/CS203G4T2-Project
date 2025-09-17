cad102537ff534aeca88570616c28875b3c2a87c
Kayweesim <kaywee.sim.2024@smu.edu.sg>
2025-09-17 16:54:49 +0800

    Added .env plugin

--- src/main/java/com/cs205/tariffg4t2/Tariffg4t2Application.java
+++ src/main/java/com/cs205/tariffg4t2/Tariffg4t2Application.java
@@ -3,9 +3,11 @@ package com.cs205.tariffg4t2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.CommandLineRunner;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class Tariffg4t2Application implements CommandLineRunner {
+    private final Dotenv dotenv = Dotenv.load();
 
     public static void main(String[] args) {
         SpringApplication.run(Tariffg4t2Application.class, args);
@@ -14,9 +16,8 @@ public class Tariffg4t2Application implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("Testing");
-
-
+        System.out.println("db username is " + dotenv.get("DB_USERNAME"));
+        System.out.println("db password is " + dotenv.get("DB_PASSWORD"));
     }
 
 }
